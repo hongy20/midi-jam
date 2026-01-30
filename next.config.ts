c05c8ce18dc0a956bca240f7370928580f7d3420
Yan Hong <yan.hong.cn@gmail.com>
2026-01-30 14:25:24 +0100

    chore(config): allow local network development
    Add `allowedDevOrigins` to `next.config.ts` to enable access from local network devices (e.g., mobile phones) during development.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    // @ts-expect-error - This feature is available but might not be in the type definition yet
+    browserDebugInfoInTerminal: true,
+    allowedDevOrigins: ["192.168.1.186:3000"],
+  },
 };
 
 export default nextConfig;
