7d2e567cb7ef3f7d9d878189467e7054296d2aee
Yan Hong <yan.hong.cn@gmail.com>
2026-01-30 14:27:42 +0100

    fix(config): move allowedDevOrigins to root

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["192.168.1.186:3000"],
   experimental: {
     // @ts-expect-error - This feature is available but might not be in the type definition yet
     browserDebugInfoInTerminal: true,
-    allowedDevOrigins: ["192.168.1.186:3000"],
   },
 };
 
