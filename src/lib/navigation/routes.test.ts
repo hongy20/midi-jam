d71689e1086d055ddfef159b9cfbf55c4e95566e
Yan Hong <yan.hong.cn@gmail.com>
2026-03-06 12:09:00 +0100

    test: simplify route definitions test by removing checks for legacy keys

--- src/lib/navigation/routes.test.ts
+++ src/lib/navigation/routes.test.ts
@@ -2,17 +2,6 @@ import { expect, test } from "vitest";
 import { ROUTES } from "./routes";
 
 test("ROUTES should match the new arcade naming scheme", () => {
-  // @ts-ignore - checking that old keys are gone
-  expect(ROUTES.TRACKS).toBeUndefined();
-  // @ts-ignore
-  expect(ROUTES.INSTRUMENTS).toBeUndefined();
-  // @ts-ignore
-  expect(ROUTES.GAME).toBeUndefined();
-  // @ts-ignore
-  expect(ROUTES.RESULTS).toBeUndefined();
-  // @ts-ignore
-  expect(ROUTES.SETTINGS).toBeUndefined();
-
   expect(ROUTES.COLLECTION).toBe("/collection");
   expect(ROUTES.GEAR).toBe("/gear");
   expect(ROUTES.PLAY).toBe("/play");
