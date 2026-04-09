9e18294a0f24b452b6ed1e84bcf78835863687ef
Yan Hong <yan.hong.cn@gmail.com>
2026-04-09 12:33:55 +0200

    fix(stories): add missing props to fix CI build

--- src/app/welcome-page.view.stories.tsx
+++ src/app/welcome-page.view.stories.tsx
@@ -7,6 +7,9 @@ const meta: Meta<typeof WelcomePageView> = {
   parameters: {
     layout: "fullscreen",
   },
+  args: {
+    loadingTimeout: 1000,
+  },
   argTypes: {
     onStart: { action: "onStart" },
     onOptions: { action: "onOptions" },
