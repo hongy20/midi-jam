75dae32fdd8f7167423fa5807b0f88d5003dd6e4
Yan Hong <yan.hong.cn@gmail.com>
2026-04-10 13:51:19 +0200

    style: fix carousel viewport safety using outer gutter wrapper

--- src/app/gear/components/gear-page.view.tsx
+++ src/app/gear/components/gear-page.view.tsx
@@ -45,34 +45,36 @@ export function GearPageView({
             description="Play a note on your gear to select it, or tap a card below."
           />
 
-          <Carousel
-            className="mx-auto w-full max-w-4xl px-12 md:px-16"
-            opts={{ align: "start", loop: false }}
-          >
-            <CarouselContent>
-              {inputs.map((input, index) => (
-                <CarouselItem
-                  className="pl-4 basis-[100%] sm:basis-1/2 lg:basis-1/3"
-                  key={input.id}
-                >
-                  <GearCard
-                    title={input.name || "Unknown Device"}
-                    description={input.manufacturer || "Generic MIDI Input"}
-                    icon={<Piano className="size-8" />}
-                    isSelected={selectedItemIndex === index}
-                    onClick={() => onSelect(input)}
-                    badge={
-                      input.id === selectedMIDIInput?.id
-                        ? "SELECTED"
-                        : undefined
-                    }
-                  />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
+          <div className="w-full px-12 md:px-16 overflow-visible">
+            <Carousel
+              className="mx-auto w-full max-w-4xl"
+              opts={{ align: "start", loop: false }}
+            >
+              <CarouselContent>
+                {inputs.map((input, index) => (
+                  <CarouselItem
+                    className="pl-4 basis-[100%] sm:basis-1/2 lg:basis-1/3"
+                    key={input.id}
+                  >
+                    <GearCard
+                      title={input.name || "Unknown Device"}
+                      description={input.manufacturer || "Generic MIDI Input"}
+                      icon={<Piano className="size-8" />}
+                      isSelected={selectedItemIndex === index}
+                      onClick={() => onSelect(input)}
+                      badge={
+                        input.id === selectedMIDIInput?.id
+                          ? "SELECTED"
+                          : undefined
+                      }
+                    />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
         </>
       )}
 
