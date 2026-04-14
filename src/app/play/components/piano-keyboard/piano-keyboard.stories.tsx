25757212a29339af795c67407e02b3fb9fa8a246
Yan Hong <yan.hong.cn@gmail.com>
2026-04-14 12:25:48 +0200

    fix: storybook docs layout for piano keyboard

--- src/app/play/components/piano-keyboard/piano-keyboard.stories.tsx
+++ src/app/play/components/piano-keyboard/piano-keyboard.stories.tsx
@@ -14,7 +14,7 @@ const meta: Meta<typeof PianoKeyboard> = {
         style={
           {
             width: "100%",
-            height: "100dvh",
+            height: "400px",
             background: "var(--background)",
             display: "flex",
             flexDirection: "column",
