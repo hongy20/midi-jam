5dc9e2754b9149803f8e496eda10f2451b11ce50
Yan Hong <yan.hong.cn@gmail.com>
2026-04-16 19:16:31 +0200

    refactor: remove redundant ThemeType export

--- src/features/theme/index.ts
+++ src/features/theme/index.ts
@@ -2,4 +2,3 @@
 export { ThemeProvider, useTheme } from "./context/theme-context";
 export { ThemePicker } from "./components/theme-picker";
 export { Theme } from "./lib/themes";
-export type { Theme as ThemeType } from "./lib/themes";
