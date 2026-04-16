b48a5b6b8be61ff9a0a0cd428368099ac5f32695
Yan Hong <yan.hong.cn@gmail.com>
2026-04-16 19:54:47 +0200

    refactor: further encapsulation of hardware feature internals

--- src/features/midi-hardware/index.ts
+++ src/features/midi-hardware/index.ts
@@ -1,8 +1,3 @@
 export { GearProvider, useGear } from "./context/gear-context";
-export {
-  getMIDIInputDevices,
-  getMIDIOutputDevices,
-  onMIDIDevicesStateChange,
-} from "./lib/midi-devices";
 export type { MIDINoteEvent } from "./lib/midi-listener";
 export { subscribeToNotes } from "./lib/midi-listener";
