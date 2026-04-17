e036b3855f2ae18fe85f15b89e350c9a339d5813
Yan Hong <yan.hong.cn@gmail.com>
2026-04-17 08:59:08 +0200

    chore: delete unused score-storage file

--- src/features/score/index.ts
+++ src/features/score/index.ts
@@ -1,5 +1,3 @@
-export type { ScoreContextType, SessionResults } from "./context/score-context";
 export { ScoreProvider, useScore } from "./context/score-context";
 export type { HitQuality } from "./hooks/use-score-engine";
 export { useScoreEngine } from "./hooks/use-score-engine";
-export { calculateNoteWeights } from "./lib/score-engine";
