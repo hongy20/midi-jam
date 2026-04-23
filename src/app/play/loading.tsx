f7b0be2de01664ebf473243fe30a0a480385d932
Yan Hong <yan.hong.cn@gmail.com>
2026-04-23 14:12:26 +0200

    refactor: remove redundant play/loading.tsx in favor of global loading boundary

--- src/app/play/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingScreen from "@/shared/components/ui/8bit/blocks/loading-screen";
-
-export default function PlayLoading() {
-  return <LoadingScreen variant="fullscreen" title="PREPARING STAGE" autoProgress />;
-}
