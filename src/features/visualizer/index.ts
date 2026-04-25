b7d8f2e41c187fa0b15a1f944a0a2381af15a9e1
Yan Hong <yan.hong.cn@gmail.com>
2026-04-25 16:53:19 +0200

    style: fix export sorting in visualizer index

--- src/features/visualizer/index.ts
+++ src/features/visualizer/index.ts
@@ -1,3 +1,3 @@
-export { Timeline } from "./components/timeline/Timeline";
 export type { TimelineProps } from "./components/timeline/Timeline";
+export { Timeline } from "./components/timeline/Timeline";
 export { LANE_SCROLL_DURATION_MS } from "./lib/constants";
