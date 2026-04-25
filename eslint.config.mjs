8273e44e7b33c235a8afb03fcc0e97ece925fef8
Yan Hong <yan.hong.cn@gmail.com>
2026-04-25 19:02:43 +0200

    style: enforce import grouping at top of file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,9 @@ const eslintConfig = [
     rules: {
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
     },
   },
   {
